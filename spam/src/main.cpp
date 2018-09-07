d1d580adcab82c6e78f5596b167a5030b9f79de1
muneeral <CodingEngcpp@gmail.com>
2018-09-07 05:17:36 -0700

    adding all files

---
+++ spam/src/main.cpp
@@ -0,0 +1,42 @@
+#include "include.h"
+
+
+PyObject* some_function1(PyObject* self, PyObject* args) {
+	__int64 input_value;
+	if (!PyArg_ParseTuple(args, "L", &input_value)) {
+		goto error;
+	}
+error:
+	return PyLong_FromLongLong(input_value + 1);
+}
+
+PyObject* some_function2(PyObject* self, PyObject* args) {
+	__int64 input_value;
+	if (!PyArg_ParseTuple(args, "L", &input_value)) {
+		goto error;
+	}
+error:
+	return PyLong_FromLongLong(input_value + 2);
+}
+
+
+PyMethodDef SpamMethods[]{
+	{"add_one", (PyCFunction)some_function1, METH_VARARGS, 0},
+	{"add_two", (PyCFunction)some_function2, METH_VARARGS, 0},
+	{0,0,0,0}
+};
+
+static struct PyModuleDef spammodule = {
+	PyModuleDef_HEAD_INIT,
+	"spam",   /* name of module */
+	"My Wonderful module spam", /* module documentation, may be NULL */
+	-1,       /* size of per-interpreter state of the module,
+				 or -1 if the module keeps state in global variables. */
+	SpamMethods
+};
+
+PyMODINIT_FUNC
+PyInit_spam(void)
+{
+	return PyModule_Create(&spammodule);
+}
---
+++ spam/test.py
@@ -0,0 +1,7 @@
+import spam
+
+print( dir(spam))
+
+print(spam.__doc__)
+
+print( spam.add_one(1234) )
